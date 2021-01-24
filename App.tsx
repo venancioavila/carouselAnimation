d1ae43e66e2ff1a3e536757c0a339e3e91eeb6b4
venancioavila <venanciodesouza@icloud.com>
2021-01-24 10:32:17 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,86 @@
+// Inspiration: https://dribbble.com/shots/14139308-Simple-Scroll-Animation
+// Illustrations by: SAMji https://dribbble.com/SAMji_illustrator
+
+import * as React from 'react';
+import {useRef} from 'react';
+import {
+  StatusBar,
+  Image,
+  Animated,
+  View,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
+const {width} = Dimensions.get('screen');
+
+const data = [
+  'https://cdn.dribbble.com/users/3281732/screenshots/7284562/media/d65a4ce1bc2754d4a94b4884ae4c90dc.jpg?compress=1&resize=1200x1200',
+  'https://cdn.dribbble.com/users/3281732/screenshots/7494265/media/59c910cbe462cb632449d5c464684555.jpg?compress=1&resize=1200x1200',
+  'https://cdn.dribbble.com/users/3281732/screenshots/7012328/media/bcd672685071ca4da27d5f3ea44ac5db.jpg?compress=1&resize=1200x1200',
+  'https://cdn.dribbble.com/users/3281732/screenshots/14012664/media/eff7aa5c75262a9cf85f897e8ef75113.jpeg?compress=1&resize=1200x1200',
+  'https://cdn.dribbble.com/users/3281732/screenshots/13130602/media/592ccac0a949b39f058a297fd1faa38e.jpg?compress=1&resize=1200x1200',
+  'https://cdn.dribbble.com/users/3281732/screenshots/6103806/b5vodvow.png?compress=1&resize=1200x1200',
+  'https://cdn.dribbble.com/users/3281732/screenshots/13661330/media/1d9d3cd01504fa3f5ae5016e5ec3a313.jpg?compress=1&resize=1200x1200',
+];
+
+const imageW = width * 0.7;
+const imageH = imageW * 1.54;
+
+export default () => {
+  const scrollX = useRef(new Animated.Value(0)).current;
+
+  return (
+    <View style={{flex: 1, backgroundColor: '#000'}}>
+      <StatusBar hidden />
+      <View style={[StyleSheet.absoluteFillObject]}>
+        {data.map((image, index) => {
+          const inputRange = [
+            (index - 1) * width,
+            index * width,
+            (index + 1) * width,
+          ];
+
+          const opacity = scrollX.interpolate({
+            inputRange,
+            outputRange: [0, 1, 0],
+          });
+
+          return (
+            <Animated.Image
+              key={image + index}
+              source={{uri: image}}
+              style={[StyleSheet.absoluteFillObject, {opacity}]}
+              blurRadius={50}
+            />
+          );
+        })}
+      </View>
+      <Animated.FlatList
+        data={data}
+        horizontal
+        pagingEnabled
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {x: scrollX}}}],
+          {useNativeDriver: true},
+        )}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({item}) => {
+          return (
+            <View
+              style={{width, justifyContent: 'center', alignItems: 'center'}}>
+              <Image
+                source={{uri: item}}
+                style={{
+                  width: imageW,
+                  height: imageH,
+                  resizeMode: 'cover',
+                  borderRadius: 16,
+                }}
+              />
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+};
